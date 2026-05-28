87d1fa88ee2bdb7665757fd0b1267f7b87599d8b
gabrielhmoura7-sudo <gabriel.h.moura7@aluno.senai.br>
2026-05-28 14:20:52 -0300

    gg

--- BACK/ATV/Controllers/CanalController.cs
+++ BACK/ATV/Controllers/CanalController.cs
@@ -1,6 +1,93 @@
-﻿namespace ATV.Controllers
+﻿using BancoIFTT.Data;
+using BancoIFTT.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BancoIFTT.Controllers
 {
-    public class CanalController
+    [ApiController]
+    [Route("[controller]")]
+    public class CanalController : ControllerBase
     {
+        private readonly ifttDBContext _context;
+
+        public CanalController(ifttDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult ListarCanais()
+        {
+            var canais = _context.Canais.ToList();
+
+            return Ok(canais);
+        }
+
+        [HttpPost]
+        public IActionResult CriarCanal(Canal canal)
+        {
+            _context.Canais.Add(canal);
+
+            _context.SaveChanges();
+
+            return Ok("Canal criado com sucesso");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarCanal(int id)
+        {
+            var canal = _context.Canais.Find(id);
+
+            if (canal == null)
+            {
+                return NotFound("Canal não encontrado");
+            }
+
+            return Ok(canal);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizarCanal(int id, Canal canal)
+        {
+            var canalBanco = _context.Canais.Find(id);
+
+            if (canalBanco == null)
+            {
+                return NotFound("Canal não encontrado");
+            }
+
+            canalBanco.Nome_Canal = canal.Nome_Canal;
+
+            _context.SaveChanges();
+
+            return Ok("Canal atualizado");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletarCanal(int id)
+        {
+            var canal = _context.Canais.Find(id);
+
+            if (canal == null)
+            {
+                return NotFound("Canal não encontrado");
+            }
+
+            _context.Canais.Remove(canal);
+
+            _context.SaveChanges();
+
+            return Ok("Canal deletado");
+        }
+
+        [HttpGet("{id}/mensagens")]
+        public IActionResult ListarMensagensCanal(int id)
+        {
+            var mensagens = _context.Comunicacao_Mensagens
+                .Where(m => m.Fk_Canal_Id_Canal == id)
+                .ToList();
+
+            return Ok(mensagens);
+        }
     }
 }
